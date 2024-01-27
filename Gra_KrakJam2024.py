ec7066e581819ec7be4c2dd9fcb8e3997d2dfa49
WhiteCrosses <szymon.modrzynski@gmail.com>
2024-01-27 01:17:59 +0100

    Moved game loop to function and updated screen dimensions in calls

--- Gra_KrakJam2024.py
+++ Gra_KrakJam2024.py
@@ -16,7 +16,7 @@ color_light = (170, 170, 170)
 # dark shade of the button
 color_dark = (80, 80, 80)
 
-is_running = True
+
 
 pygame.font.init()
 
@@ -24,44 +24,46 @@ font = pygame.font.SysFont("Comic Sans MS", 34)
 start_game = font.render("Play", False, start_color)
 quit_game = font.render("Quit", False, start_color)
 
-while is_running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_running = False
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-
-            #If the mouse is clicked on the button the game is terminated
-            if screen_width/2 <= mouse[0] <= screen_width/2 + 130 and screen_height / 2 <= mouse[1] <= screen_height / 2 + 40:
-                #Move to the next stae in the game
-                #is_running = False
-                print("Play")
-
-            # If the mouse is clicked on the button the game is terminated
-            if screen_width/2 <= mouse[0] <= screen_width/2 + 130 and screen_height / 2 + 60 <= mouse[1] <= screen_height / 2 + 120:
-                # Move to the next stae in the game
+def game_loop():
+    is_running = True
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 is_running = False
 
-    window.fill((0, 0, 0))
-    mouse = pygame.mouse.get_pos()
+            if event.type == pygame.MOUSEBUTTONDOWN:
 
-    #If mouse is hovered on a button it changes to lighter shade
-    if screen_width/2 <= mouse[0] <= screen_width/2 + 130 and screen_height/2 <= mouse[1] <= screen_height/2 + 40:  
-        pygame.draw.rect(window,color_light,[screen_width/2 - 120,screen_height/2,260,50])    
-    else:  
-        pygame.draw.rect(window,color_dark,[screen_width/2 - 120,screen_height/2,260,50]) 
+                #If the mouse is clicked on the button the game is terminated
+                if WINDOW_WIDTH/2 <= mouse[0] <= WINDOW_WIDTH/2 + 130 and WINDOW_HEIGHT / 2 <= mouse[1] <= WINDOW_HEIGHT / 2 + 40:
+                    #Move to the next stae in the game
+                    #is_running = False
+                    print("Play")
 
-    #If mouse is hovered on a button it changes to lighter shade
-    if screen_width/2 <= mouse[0] <= screen_width/2 + 130 and screen_height/2 + 60 <= mouse[1] <= screen_height/2 + 120:  
-        pygame.draw.rect(window,color_light,[screen_width/2 - 120,screen_height/2 + 60,260,80])  
-    else:  
-        pygame.draw.rect(window,color_dark,[screen_width/2 - 120,screen_height/2 + 60,260,80]) 
+                # If the mouse is clicked on the button the game is terminated
+                if WINDOW_WIDTH/2 <= mouse[0] <= WINDOW_WIDTH/2 + 130 and WINDOW_HEIGHT / 2 + 60 <= mouse[1] <= WINDOW_HEIGHT / 2 + 120:
+                    # Move to the next stae in the game
+                    is_running = False
 
-    window.blit(start_game, (screen_width/2 - 40,screen_height/2))
-    window.blit(quit_game, (screen_width/2 - 40, screen_height/2 + 60))
-    # Update the frames of the screen
-    pygame.display.update()
-    pygame.display.flip()
+        window.fill((0, 0, 0))
+        mouse = pygame.mouse.get_pos()
 
-pygame.quit()
-sys.exit()
+        #If mouse is hovered on a button it changes to lighter shade
+        if WINDOW_WIDTH/2 <= mouse[0] <= WINDOW_WIDTH/2 + 130 and WINDOW_HEIGHT/2 <= mouse[1] <= WINDOW_HEIGHT/2 + 40:  
+            pygame.draw.rect(window,color_light,[WINDOW_WIDTH/2 - 120,WINDOW_HEIGHT/2,260,50])    
+        else:  
+            pygame.draw.rect(window,color_dark,[WINDOW_WIDTH/2 - 120,WINDOW_HEIGHT/2,260,50]) 
+
+        #If mouse is hovered on a button it changes to lighter shade
+        if WINDOW_WIDTH/2 <= mouse[0] <= WINDOW_WIDTH/2 + 130 and WINDOW_HEIGHT/2 + 60 <= mouse[1] <= WINDOW_HEIGHT/2 + 120:  
+            pygame.draw.rect(window,color_light,[WINDOW_WIDTH/2 - 120,WINDOW_HEIGHT/2 + 60,260,80])  
+        else:  
+            pygame.draw.rect(window,color_dark,[WINDOW_WIDTH/2 - 120,WINDOW_HEIGHT/2 + 60,260,80]) 
+
+        window.blit(start_game, (WINDOW_WIDTH/2 - 40,WINDOW_HEIGHT/2))
+        window.blit(quit_game, (WINDOW_WIDTH/2 - 40, WINDOW_HEIGHT/2 + 60))
+        # Update the frames of the screen
+        pygame.display.update()
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
